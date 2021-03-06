0c46f74381c1006afe2ae46370f0fee24de1e240
richi <richi.ramadhan@fore.coffee>
2021-03-06 11:49:04 +0700

    penambahan env timeout untuk seluruh scenario

--- tests/login.js
+++ tests/login.js
@@ -1,19 +1,24 @@
 Feature('Login');
 
 Scenario('Sukses Login', ({ I }) => {
+    // buka homepage
     I.amOnPage('/')
+    // click sign in
     I.click('Sign in')
-    I.waitForText('Need an account?', 50)
+    // verify text
+    I.waitForText('Need an account?', process.env.TIMEOUT)
+    // mengisi form login
     I.fillField({css:'input[placeholder=Email]'}, 'poiqwe1234524@gmail.com')
     I.fillField({css:'input[placeholder=Password]'}, 'poiqwe123456')
     I.click({css:'button[type=submit]'})
+    // verify 
     I.see('richirin')
 });
 
 Scenario('Gagal Login', ({ I }) => {
     I.amOnPage('/')
     I.click('Sign in')
-    I.waitForText('Need an account?', 50)
+    I.waitForText('Need an account?', process.env.TIMEOUT)
     I.fillField({css:'input[placeholder=Email]'}, 'asalin')
     I.fillField({css:'input[placeholder=Password]'}, 'poiqwe123456')
     I.click({css:'button[type=submit]'})
